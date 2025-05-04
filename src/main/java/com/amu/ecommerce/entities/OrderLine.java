1d0f4f9c3c627d865b08317b51534a469d3ccd75
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-04 15:21:44 +0700

    feat(order): add Order, OrderLine entities and PaymentMethod enum

---
+++ src/main/java/com/amu/ecommerce/entities/OrderLine.java
@@ -0,0 +1,23 @@
+package com.amu.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    Order order;
+    Integer productId;
+    Double quantity;
+}
