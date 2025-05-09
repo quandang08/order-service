43faaa64bbcd864abdb12f8c65f861d55537c727
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-09 21:21:04 +0700

    feat(order-service): Implement the exception handling.

---
+++ src/main/java/com/amu/ecommerce/handler/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.amu.ecommerce.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ErrorResponse {
+    Map<String,String> errors;
+
+    public ErrorResponse(HashMap<String, String> errors) {
+    }
+}
