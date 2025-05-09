d21998e28d1a65f3e523cc35697d0e19bec9a9a3
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-09 21:18:30 +0700

    feat: Add OrderLineRepository for database interaction

--- src/main/java/com/amu/ecommerce/repositories/OrderLineRepository.java
+++ src/main/java/com/amu/ecommerce/repositories/OrderLineRepository.java
@@ -1,7 +1,11 @@
 package com.amu.ecommerce.repositories;
 
+import com.amu.ecommerce.dto.OrderLineResponse;
 import com.amu.ecommerce.entities.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
+    List<OrderLine> findAllByOrderId(Integer orderId);
 }
